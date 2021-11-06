9a179b21b7a1f61bfb44d7870bdb268f86242ba8
Helga <89998178494@mail.ru>
2021-11-07 02:26:49 +0300

    Добавьте файлы проекта.

---
+++ inheritance/Man.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance
+{
+    class Man
+    {
+        public Man(string name, int age)
+        {
+            Name = name;
+            setAge(age);
+        }
+        private int age;
+        private string Name { get; set; }
+
+        public int getThisAge()
+        {
+            return this.age;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setAge(int age )
+        {
+           this.age=age;
+        }
+        
+        public void Greet()
+        {
+            Console.WriteLine("Привет ! Меня зовут " + Name + ", мне " + age + " лет.");
+        }
+    }
+}
---
+++ inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man man1 = new Man("Витя", 45);
+            man1.Greet();
+
+            Console.ReadKey();
+        }
+    }
+}
