824362ef3655036e7900c64020e8304b2e3c54d1
Helga <89998178494@mail.ru>
2021-11-12 23:26:41 +0300

    Добавлен класс Woman

--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -8,11 +8,16 @@ namespace inheritance
         {
             Man man1 = new Man("Витя", 45);
             Man man2 = new Man("Оля алкаш", 25);
+            Woman woman1 = new Woman("Марина", 78);
             man1.Greet();
             man1.setAge(66);
             man1.Greet();
             man2.Greet();
             man1.setAge(46);
+            woman1.Greet();
+            woman1.setAge(78);
+
+
             // man1.HappyBirthday(); man1.HappyBirthday(); man1.HappyBirthday();
             Console.WriteLine(Man.RETIREMENT_AGE);
 
--- inheritance/Woman.cs
+++ inheritance/Woman.cs
@@ -6,5 +6,42 @@ namespace inheritance
 {
     class Woman
     {
+        public Woman(string name, int age)
+        {
+            Name = name;
+            setAge(age);
+            setIsRetired();
+        }
+        private int age;
+        public string Name { get; set; }
+
+        private bool isRetired;
+        public static int RETIREMENT_AGE = 60;
+        public bool getIsRetired()
+        {
+            return isRetired;
+        }
+        public void setIsRetired()
+        {
+            isRetired = (age >= RETIREMENT_AGE) ? true : false;
+        }
+
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+            setIsRetired();
+        }
+
+        public void Greet()
+        {
+            //setAge(age + 1);
+            Console.WriteLine("Привет ! Меня зовут " + Name + ", мне " + age + " лет. " + (isRetired ? "Я на пенсии" : "Я не на пенсии"));
+        }
     }
 }
